0c8a44d9a36ae461377684ae23679d39217112d1
Pei-Chi Huang <45569842+unocps@users.noreply.github.com>
2021-02-01 21:57:20 +0800

    Add files via upload

--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -40,14 +40,7 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=False)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+#XXXX TODO
 
 if __name__ == '__main__':
     try:
